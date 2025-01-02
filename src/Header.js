e80108c12cb1a02aac2afe62c936111dee7d53be
Kaven Muraleitharan <kavenmuraleitharan0726@gmail.com>
2025-01-01 20:11:01 -0500

    react app created

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <div className="header__wrapper">
+        {/*Logo*/}
+        TEST
+        {/*search*/}
+        {/*menuitems*/}
+      </div>
+    </div>
+  )
+}
+
+export default Header
